167a5be4ff936c0c5dfae5615c9da7dea43f44d4
Thanh Bui <ttthanhbui@gmail.com>
2019-04-06 15:03:19 -0600

    Add logout to Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,9 +4,6 @@ import { Menu, Image, Container, Button } from 'semantic-ui-react'
 import { Link, withRouter, } from 'react-router-dom'
 import styled from 'styled-components'
 
-const logo = 'https://resources-live.sketch.cloud/files/0c691bf6-95ea-4f35-ad3e-46e842eefe7b.png?Expires=1554685200&Signature=mCOAD2p5sijiSsM4HWpafP-cQs4TRHAouci4hZ3tLuXuA~Rs1Fqe8qYhPw3PvbOg2~vl0hr98Uy3ElhQY5~cO9wXDPzYtAR0lrlBZoFTBFmEkK6Qc8LuZfloXO8sT~b-bmR9xhm1v0mGDwIhJABaOqlq6xbKgBGyjfc-0CVJRXE_&Key-Pair-Id=APKAJOITMW3RWOLNNPYA'
-const avatar = 'https://resources-live.sketch.cloud/files/6f304d0b-fd53-4d76-8fa4-3bbd49f2b696.png?Expires=1554685200&Signature=htQQ86E9s68e~-DlOp1k2kmORHfmxk3sZo3rVzMZaskEFSeE1ayDltK~1KCQ2V7esIq5l0Vcqf9WPyCPzJKkR~rhwlqjzXgE74DATtCvSCmNIQ28ru61dI5WKU~T3VfeanYnSkujS623uOF1aF92THVMWHWNWOh8qZOZMwPuhVk_&Key-Pair-Id=APKAJOITMW3RWOLNNPYA'
-
 class Navbar extends React.Component {
 
   rightNavItems = () => {
@@ -77,35 +74,6 @@ class Navbar extends React.Component {
   render() {
     
     return (
-<<<<<<< HEAD
-      <div>
-        <Menu pointing secondary>
-          <Menu.Menu>
-            <Link to='/'>
-              <Menu.Item
-                name='home'
-                id='home'
-                active={this.props.location.pathname === '/'}
-              >
-                <Image src={logo} size='tiny' />
-              </Menu.Item>
-            </Link>
-          </Menu.Menu>
-          <Menu.Menu position='right'>
-            <Link to='/profile'>
-              <Menu.Item
-                name='profile'
-                id='profile'
-                active={this.props.location.pathname === '/profile'}
-              >
-                <Image src={avatar} style={{ width: '1cm', height: '1cm' }} />
-              </Menu.Item>
-            </Link>
-            {this.rightNavItems()}
-          </Menu.Menu>
-        </Menu>
-      </div>
-=======
       <StyledMenu pointing secondary>
         <Container>
             <Menu.Item
@@ -122,7 +90,6 @@ class Navbar extends React.Component {
             { this.rightNavItems() }
         </Container>  
       </StyledMenu>
->>>>>>> 808e1d4886071f870dee2c6eac5dd2b94c280d78
     )
   }
 }
