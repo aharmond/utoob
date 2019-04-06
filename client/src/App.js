c8672e97aa6a9945c4827fee21d78a71d2b422be
AJ Harmond <ajharmond@gmail.com>
2019-04-06 14:42:59 -0600

    Video view page is working, also restructured the components folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Home from './components/Home';
 import NoMatch from './components/NoMatch';
 import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import UserView from './components/usercomponents/UserView';
 import UserProfile from './components/usercomponents/UserProfile';
-import FetchUser from './components/FetchUser';
-import ProtectedRoute from './components/ProtectedRoute';
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 import { Switch, Route, } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { Container, } from "semantic-ui-react";
-import VideoForm from './components/VideoForm';
+import VideoForm from './components/videocomponents/VideoForm';
+import VideoPage from './components/videocomponents/VideoPage';
 
 const App = () => (
   <>
@@ -26,6 +27,7 @@ const App = () => (
           <Route exact path="/register" component={Register} />
           <ProtectedRoute exact path="/user/:id" component={UserView} />
           <ProtectedRoute exact path="/videos/new" component={VideoForm} />
+          <Route exact path='/users/:user_id/videos/:id' component={VideoPage} />
           <ProtectedRoute exact path="/videos/:id/edit" component={VideoForm} />
           <Route component={NoMatch} />
         </Switch>
