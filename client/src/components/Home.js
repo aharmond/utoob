f62902cc27bf5cd91a57926afd3a4778cde642d8
Thanh Bui <ttthanhbui@gmail.com>
2019-04-06 14:06:01 -0600

    delete VideoView

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Header, } from 'semantic-ui-react';
 import Comments from './Comments';
-import VideoView from './VideoView';
 
 const Home = () => (
   <div>
   <br />
-  <VideoView />
   <Comments />
   </div>
 )
